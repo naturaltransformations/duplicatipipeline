47fa9bcbbfd69bebdb8fc43669ca6e1196a8b66f
verhoek <30193551+verhoek@users.noreply.github.com>
2018-12-19 08:15:39 +0100

    fixes.

--- stage_build/wrapper.sh
+++ stage_build/wrapper.sh
@@ -7,4 +7,4 @@ run "$@" --dockerimage mono \
 --dockerpackages "$PACKAGES" \
 --sourcecache "$DUPLICATI_ROOT" \
 --targetcache "$BUILD_CACHE" \
---dockercommand "./BuildTools/PipeLine/stage_build/build.sh $FORWARD_OPTS"
+--dockercommand "./BuildTools/PipeLine/stage_build/build.sh"
--- stage_unittests/wrapper.sh
+++ stage_unittests/wrapper.sh
@@ -4,12 +4,9 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # rename to config.sh
 PACKAGES="wget unzip rsync"
-parse_options "$@" \
+run "$@" \
 --dockerimage mono \
 --dockerpackages "$PACKAGES" \
 --sourcecache "$BUILD_CACHE" \
 --targetcache "$TEST_CACHE" \
 --dockercommand "./BuildTools/PipeLine/stage_unittests/test.sh"
-
-pull_docker_image
-run_with_docker
