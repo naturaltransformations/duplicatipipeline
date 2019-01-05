25708b94438b361d6b2cc0e1faa995dc2252571f
verhoek <30193551+verhoek@users.noreply.github.com>
2019-01-05 17:00:34 +0100

    Remove custom docker image due to failing build for arm cross compilation.

--- pipeline/stage_createinstallers/trigger.sh
+++ pipeline/stage_createinstallers/trigger.sh
@@ -3,5 +3,6 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 . "${SCRIPT_DIR}/../shared/error_handling.sh"
 
 "${SCRIPT_DIR}/../shared/start_docker.sh" "$@" \
---dockerimage naturaltransformations/ubuntu_qemu \
+--dockerimage ubuntu \
+--dockerpackages "qemu-user qemu-user-static unzip rsync bzip2 docker.io" \
 --dockercommand "/pipeline/stage_createinstallers/job.sh"
