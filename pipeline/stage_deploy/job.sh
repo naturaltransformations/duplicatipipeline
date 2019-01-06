ce1feca2b58e248d38c026da53fd88a457e92c58
verhoek <30193551+verhoek@users.noreply.github.com>
2019-01-06 10:04:11 +0100

    Added aws clean up function.

--- pipeline/stage_deploy/job.sh
+++ pipeline/stage_deploy/job.sh
@@ -2,9 +2,27 @@
 . /pipeline/docker-run/markers.sh
 . /pipeline/shared/duplicati.sh
 
-function upload_to_aws() {
+function aws_clean_up() {
+  aws s3 ls "$awsbucket/" --recursive | while read -r line;
+    do
+      createDate=`echo $line|awk {'print $1" "$2'}`
+      createDate=`date -d"$createDate" +%s`
+      olderThan=`date --date "3 days ago" +%s`
+      if [[ $createDate -lt $olderThan ]]; then
+        fileName=`echo $line|awk {'print $4'}`
+        if [[ $fileName != "" ]]; then
+          aws s3 rm "${awsbucket}"/$fileName
+        fi
+      fi
+    done;
+}
+
+function aws_load_credentials() {
 	export AWS_ACCESS_KEY_ID=$awskeyid
 	export AWS_SECRET_ACCESS_KEY=$awssecret
+}
+
+function aws_upload() {
 	aws s3 cp "${UPDATE_TARGET}/" "${awsbucket}/${releasetype}/" --recursive --exclude "docker.*"
 }
 
@@ -25,5 +43,7 @@ function push_docker () {
 
 travis_mark_begin "UPLOADING BINARIES"
 push_docker
-upload_to_aws
+aws_load_credentials
+aws_upload
+aws_clean_up
 travis_mark_end "UPLOADING BINARIES"
