1b06bff2653cc34caa8c6caa1475bf7b04ef41a3
verhoek <30193551+verhoek@users.noreply.github.com>
2019-01-11 20:02:23 +0100

    Fix wrong name causing version not to be propagated.

--- pipeline/jobs/build_job.sh
+++ pipeline/jobs/build_job.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 . "${SCRIPT_DIR}/params.sh"
 
 ${ROOT_DIR}/pipeline/stage_build/trigger.sh \
-${FORWARD_OPTIONS[@]} \
+${FORWARD_OPTS[@]} \
 --sourcedir "${ROOT_DIR}/duplicati" \
 --targetdir "${BUILD_DIR}" | ts
