48e216b39ca432d2770a72186ced7a358a52ac6d
verhoek <30193551+verhoek@users.noreply.github.com>
2019-02-06 20:38:56 +0100

    Simplified.

--- pipeline/jobs/params.sh
+++ pipeline/jobs/params.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-. "${SCRIPT_DIR}/../docker-run/error_handling.sh"
+
+export PATH="$PATH:${ROOT_DIR}/pipeline/docker-run"
+. error_handling.sh
 
 function add_option () {
   FORWARD_OPTS[${#FORWARD_OPTS[@]}]="$1"
@@ -12,6 +13,3 @@ add_option "--releaseversion" "2.0.4.$(cat "${ROOT_DIR}"/duplicati/Updates/build
 add_option "--gittag" "$(cd "${ROOT_DIR}"/duplicati;git log --pretty=format:"%h" --before="$(date -d yesterday +%F)" HEAD -n1)"
 
 export FORWARD_OPTS
-export PATH="$PATH:${ROOT_DIR}/pipeline/docker-run"
-
-. error_handling.sh
